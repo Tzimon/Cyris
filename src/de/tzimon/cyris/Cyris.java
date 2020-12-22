fb0d4a949ea18d19de9c97afca2d65c70ba16c4f
BuildTools <unconfigured@null.spigotmc.org>
2020-12-22 16:05:39 +0100

    Add essential messages and Singleton

---
+++ src/de/tzimon/cyris/Cyris.java
@@ -0,0 +1,25 @@
+package de.tzimon.cyris;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Cyris extends JavaPlugin {
+
+    private static Cyris plugin;
+
+    public String prefix = "§cCyris §8| §r";
+    public String noPlayer = "§cDu musst ein Spieler sein";
+    public String noPermission = "§cDu hast keine Berechtigung das zu tun";
+
+    public Cyris() {
+        plugin = this;
+    }
+
+    public void onEnable() {
+
+    }
+
+    public static Cyris getPlugin() {
+        return plugin;
+    }
+
+}
